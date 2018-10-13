4d6c64a317d8136957494bc613dbf4266b43fd0c
John Andersen <johnandersenpdx@gmail.com>
2018-10-12 20:08:52 -0700

    Updates

---
+++ precache-manifest.1421bb328a65320bf914bf4853fffd15.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d626e9e8f57d217751f",
+    "url": "/create-react-app/static/js/runtime~main.1d626e9e.js"
+  },
+  {
+    "revision": "8621852a3ce5828634aa",
+    "url": "/create-react-app/static/js/main.8621852a.chunk.js"
+  },
+  {
+    "revision": "8618a95a76b588a4c46f",
+    "url": "/create-react-app/static/js/1.8618a95a.chunk.js"
+  },
+  {
+    "revision": "750031b9e7de758bcdde6721076824a4",
+    "url": "/create-react-app/index.html"
+  }
+];
